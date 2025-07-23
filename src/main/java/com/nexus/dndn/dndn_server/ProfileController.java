9108ff02d1398d7f8ed66a69cff352aa35a809f3
Park, Yeonji <yeonpyj23@gmail.com>
2025-07-23 12:58:15 +0900

    test: 배포 테스트

---
+++ src/main/java/com/nexus/dndn/dndn_server/ProfileController.java
@@ -0,0 +1,31 @@
+package com.nexus.dndn.dndn_server;
+
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class ProfileController {
+
+    private final Environment env;
+
+    public ProfileController(Environment env) {
+        this.env = env;
+    }
+
+    // 현재 실행 중인 Spring Boot 애플리케이션의 활성 프로필을 반환해주는 API 컨트롤러
+    @GetMapping("/profile")
+    public String profile() {
+        List<String> profiles = Arrays.asList(env.getActiveProfiles());
+        List<String> realProfiles = Arrays.asList("real1", "real2");
+        String defaultProfile = profiles.isEmpty() ? "default" : profiles.get(0);
+
+        return profiles.stream()
+                .filter(realProfiles::contains)
+                .findAny()
+                .orElse(defaultProfile);
+    }
+}

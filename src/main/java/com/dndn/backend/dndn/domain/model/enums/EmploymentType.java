875fc3c7e0563d54848f225d150f2553b565a32f
yuliz003 <lizkjw0731@gmail.com>
2025-08-24 15:29:08 +0900

    [Feat] user 수정
    #️⃣연관된 이슈
    #25
    
    📝작업 내용
    가족 유형 삭제
    근무유형 수정
    장애 유형 추가

--- src/main/java/com/dndn/backend/dndn/domain/model/enums/EmploymentType.java
+++ src/main/java/com/dndn/backend/dndn/domain/model/enums/EmploymentType.java
@@ -1,6 +1,6 @@
 package com.dndn.backend.dndn.domain.model.enums;
 
 public enum EmploymentType {
-    EMPLOYED, FREELANCER,JOB_SEEKER,UNEMPLOYED
-    //재직 중, 프리랜서, 구직 중, 무직
+    EMPLOYED, UNEMPLOYED
+    //재직 중, 무직
 }
